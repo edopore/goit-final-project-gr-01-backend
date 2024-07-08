7f0215deecb57d94cd7d9a08cd712aab5758a90f
Eduardo Jose Maya Rodriguez <ediij1994@gmail.com>
2024-07-08 08:57:27 -0500

    UPDATE se corrige documentación en products

--- routes/api/products.js
+++ routes/api/products.js
@@ -56,23 +56,6 @@
  *                  application/json:
  *                      schema:
  *                          $ref: '#/components/error/internal' 
- * components:
- *   schemas:
- *     User:
- *       type: object
- *       required:
- *         - email
- *         - password
- *       properties:
- *         email:
- *           type: string
- *           description: email's user
- *         password:
- *           type: string
- *           description: password's user
- *       example:
- *         email: pruebamail@goitmail.com
- *         password: passwordg01t
  */
 const express = require("express");
 
