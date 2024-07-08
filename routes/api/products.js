14d4e6117d3b65af96f457ea809789f157b8b017
Eduardo Jose Maya Rodriguez <ediij1994@gmail.com>
2024-07-08 08:55:40 -0500

    UPDATE documentacion rutas products swagger

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,5 +1,61 @@
 /**
  * @swagger
+ * tags:
+ *  name: Products
+ *  description: API para administracion de productos permitidos y no permitidos
+ * products/:
+ *  get:
+ *      tags: [Products]
+ *      summary: Obtiene listado de alimentos según búsqueda
+ *      responses:
+ *          200:
+ *              description: Listado de alimentos exitoso
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          productIds:
+ *                              type: object
+ *                              description: Listado de alimentos encontrados al realizar calculo
+ *                          example:
+ *                              productIds: ["5d51694802b2373622ff5543", "5d51694802b2373622ff555c"]
+ *          401:
+ *              description: Acceso no autorizado al recurso
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          message:
+ *                              type: string
+ *                              description: mensaje de respuesta al hacer accción con usuario no autorizado.
+ *                          example:
+ *                              message: Usuario no autorizado
+
+ *          500:
+ *              description: Algún error interno en servidor
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/error/internal'
+ * productos/blacklist:
+ *  post:
+ *      tags: [Products]
+ *      summary: Obtiene listado de alimentos no recomendables para consumo de acuerdo a calculo de calorías
+ *      responses:
+ *          200:
+ *              description: Listado de alimentos exitoso
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          productIds:
+ *                              type: object
+ *                              description: Listado de alimentos encontrados al realizar calculo
+ *                          example:
+ *                              productIds: ["5d51694802b2373622ff5543", "5d51694802b2373622ff555c"] 
+ *          500:
+ *              description: Algún error interno en servidor
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/error/internal' 
  * components:
  *   schemas:
  *     User:
